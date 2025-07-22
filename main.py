520d4a76031fc16451eea46f9e4eaaf7ebcad7ae
d-vinoth-c <vinovinth19@gmail.com>
2025-07-22 11:47:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return 'Hello from Flask in Cloud Function!', 200
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    if request.is_json:
+        data = request.get_json()
+        return jsonify({
+            'received': data
+        }), 200
+    else:
+        return jsonify({'error': 'Request must be JSON'}), 400
+
+# Required by Google Cloud Functions
+def main(request):
+    return app(request)
